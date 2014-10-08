77ca9f96b8bac083130170134d24cb3a6f152d92
Xiaofan <olike.cdm@gmail.com>
2014-10-09 02:39:52 +0800

    getCommand to CommandTypes class();

--- src/parser/CommandTypes.java
+++ src/parser/CommandTypes.java
@@ -2,8 +2,15 @@ package parser;
 
 public class CommandTypes {
 
-	public CommandTypes() {
-		// TODO Auto-generated constructor stub
+	public static int SINGLEWORD = -1;
+
+	public String getCommand(String userInput) {
+		int indexFirstSpace = userInput.indexOf(' ');
+		if (indexFirstSpace == SINGLEWORD) {
+			return userInput;
+		}
+		String command = userInput.substring(0, indexFirstSpace);
+		return command;
 	}
 
 }
