ee6b4cef2a4198281843f1ea79578639812592fa
Xiaofan <olike.cdm@gmail.com>
2014-10-20 02:25:29 +0800

    Updated parser; debugging

--- src/parser/Command.java
+++ src/parser/Command.java
@@ -6,9 +6,10 @@ package parser;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Command {
-	
+
 	public static final String ON = "on";
 	public static final String AT = "at";
 	public static final String FROM = "from";
@@ -16,7 +17,7 @@ public class Command {
 	public static final String TO = "to";
 
 	public static int SINGLEWORD = -1;
-
+	
 	public static String getCommand(String userInput) {
 		int indexFirstSpace = userInput.indexOf(' ');
 		if (indexFirstSpace == SINGLEWORD) {
@@ -26,12 +27,28 @@ public class Command {
 		return command;
 	}
 
+	public static String getDescriptionOnly(String userInput){
+		try{
+			int whiteSpaceIndex = userInput.indexOf(' ');
+			return userInput.substring(whiteSpaceIndex, userInput.length());
+		}catch(IllegalArgumentException e){
+			return null;
+		}
+	}
+
+	public static Boolean isPrepPresent(String userInput){
+		if(userInput.contains(AT) || userInput.contains(BY) || userInput.contains(FROM) || userInput.contains(ON)){
+			return true;
+		}
+		return false;
+	}
+
 	public static String getDescription(String userInput){
 		String description = null;
 		int[] prepArray;
 		prepArray = new int[4];
 		Arrays.fill(prepArray, 1000);
-		
+
 		if(userInput.contains(AT)){
 			prepArray[0] = userInput.indexOf(AT);
 		}
@@ -44,16 +61,15 @@ public class Command {
 		if(userInput.contains(ON)){
 			prepArray[3] = userInput.indexOf(ON);
 		}
-		
+
 		int smallestIndex = 0;
-		
+
 		for(int i=1; i<4; i++){
 			if(prepArray[i] < prepArray[0]){
 				smallestIndex = i; 
 			}
 		}
-		
-		
+
 		if(smallestIndex == 0){
 			Pattern pattern = Pattern.compile("add(.*?\\s)at\\s");
 			Matcher matcher = pattern.matcher(userInput);
@@ -81,17 +97,39 @@ public class Command {
 		}
 		return description.trim();
 	}
-	
+
 	public static int getImportance(String userInput){
 		int count = userInput.length() - userInput.replaceAll("\\!", "").length();
-		
+
 		return count;
 	}
 	
+	public static String removeImportanceTagString(String userInput){
+		String cutUserInput1 = userInput.replaceAll("\\!", "");
+		String cutUserInput2 = cutUserInput1.replaceAll("\\s+#[^\\s]+", "");
+		return cutUserInput2;
+	}
+	
+	public static ArrayList<String> getTag(String userInput){
+		Pattern tagPattern = Pattern.compile("#(\\w+|\\w+)");
+		Matcher mat = tagPattern.matcher(userInput);
+		ArrayList<String> tags = new ArrayList<String>();
+		while(mat.find()){
+			tags.add(mat.group(1));
+		}
+		return tags;		
+	}
+
 	public static String getTypeOfTask(String userInput){
 		//"by" indicates a scheduled task
-		//"on...from...to..." indicates an event
-		//" on" indicates a normal task
-		//no prepositions indicates a normal task
+		if(userInput.contains(BY)){
+			return "scheduled task";
+		}else if(userInput.contains(ON) && userInput.contains(FROM) && userInput.contains(TO)){
+			return "event";
+		}else if(userInput.contains(ON) || userInput.contains(AT)){
+			return "task";
+		}else{
+			return "floating";
+		}
 	}
 }
