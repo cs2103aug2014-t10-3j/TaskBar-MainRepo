c37c2c0a8466fde48e93b40aca50842184441b2d
suheti <suheti.lee@gmail.com>
2014-10-30 07:40:22 +0800

    Addition to the last commit
    Might be GIT problem again? I couldn’t combine this with the last
    commit.

---
+++ src/taskbar/Command.java
@@ -0,0 +1,55 @@
+package taskbar;
+
+import java.util.ArrayList;
+
+public abstract class Command {
+	public DisplayData displayData;
+	public Storage storage;
+	public String userInput;
+	
+	public Command(DisplayData dd, Storage s, String ui){
+		displayData = dd;
+		storage = s;
+		userInput = ui;
+	}
+	
+	/**
+	 * 
+	 * @return true when the command is successfully executed; false otherwise.
+	 */
+	public abstract boolean execute();
+	
+	/*
+	 * setDisplayData method for setting WITH the need to setting input box to something.
+	 */
+	public void setDisplayData(String inputText, String prompt,
+			ArrayList<Task> listOfTasks) {
+		displayData.setNeedToUpdate(true);
+		displayData.setNeedToUpdateInputBox(true);
+		displayData.setInputText(inputText);
+		displayData.setPrompt(prompt);
+		displayData.setListOfTasks(listOfTasks);
+	}
+
+	/*
+	 * setDisplayData method for setting CLEARING the input box.
+	 */
+	public void setDisplayData(String prompt, ArrayList<Task> listOfTasks) {
+		displayData.setNeedToUpdate(true);
+		displayData.setNeedToUpdateInputBox(true);
+		displayData.setInputText("");
+		displayData.setPrompt(prompt);
+		displayData.setListOfTasks(listOfTasks);
+	}
+
+	/*
+	 * setDisplayData method for setting RETAINING the input box text, and
+	 * without the need to update the list of tasks
+	 */
+	public void setDisplayData(String prompt) {
+		displayData.setNeedToUpdate(false);
+		displayData.setNeedToUpdateInputBox(false);
+		displayData.setPrompt(prompt);
+	}
+
+}
