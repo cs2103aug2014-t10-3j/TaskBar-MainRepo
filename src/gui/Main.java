06de17367fc89d6d087ce79a1e4a4abbfaa8764a
ktoan2904 <ktoan2904@gmail.com>
2014-11-05 21:54:57 +0800

    Embed fonts, put all images and fonts in resources

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -8,6 +8,7 @@ import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 
 
 public class Main extends Application {
@@ -21,12 +22,18 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("appearance.css").toExternalForm());
 			scene.setFill(null);
 			primaryStage.setTitle("ET - Task scheduling made easy!");
-			primaryStage.getIcons().add(new Image ("icon.png"));
+			primaryStage.getIcons().add(new Image ("resources/icon.png"));
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	public void init() {
+		Font.loadFont(getClass().getResource("../resources/segoeuil.ttf").toExternalForm(), 12);
+		Font.loadFont(getClass().getResource("../resources/seguisb.ttf").toExternalForm(), 12);
 	}
 	
 	public static void main(String[] args) {
